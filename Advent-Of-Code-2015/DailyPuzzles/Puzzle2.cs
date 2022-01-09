9e15ef8c68f79ea23fa6b66ba905a0fae97741ab
Ben Kerns <benkerns@pm.me>
2022-01-08 20:14:37 -0700

    Puzzle2 preliminary setup

---
+++ Advent-Of-Code-2015/DailyPuzzles/Puzzle2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_Of_Code_2015.DailyPuzzles
+{
+    internal class Puzzle2
+    {
+        // Properties
+        public string PuzzleInput { get; set; }
+        public List<string> DistinguishedInputs { get; set; }
+        public List<int> IndividualAreas { get; set; }
+
+        // Constructors
+
+        // Methods
+    }
+}
